d50368bbca3048bbe13034bf1a9ef456258715d4
Kuragin Ivan <curagin.vanya@yandex.ru>
2024-04-12 09:53:36 +0300

    end 4 step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from dotenv import load_dotenv
 from validators import url as is_correct
 import os
 import psycopg2
 from urllib.parse import urlparse
-from datetime import datetime
+from datetime import date
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
 
 @app.get('/')
 def index():
@@ -25,21 +24,31 @@ def add_url():
         spread_url = urlparse(url)
         prepared_url = '://'.join([spread_url.scheme, spread_url.netloc])
         with conn.cursor() as cursor:
-            if len(prepared_url) < 255:
+            cursor.execute("""SELECT name FROM urls
+            WHERE name = %s;""", (prepared_url,))
+            added_earlier = cursor.fetchall()
+            if not added_earlier and len(prepared_url) < 255:
                 cursor.execute("""
                     INSERT INTO urls (name, created_at)
                     VALUES (%s, %s);
-                    """, (url, datetime.today()))
+                    """, (url, date.today()))
                 conn.commit()
-    return redirect(url_for('show_all_urls'), code=200)
-
+                flash('Страница успешно добавлена', 'info')
+                return redirect(url_for('show_all_urls'), code=200)
+            else:
+                flash('Страница уже добавлена', 'error')
+                return redirect(url_for('show_all_urls'), code=200)
+    flash('Некорректный URL', 'error')
+    return redirect(url_for('index'))
 @app.get('/urls')
 def show_all_urls():
     conn = psycopg2.connect(DATABASE_URL)
     with conn.cursor() as cursor:
         cursor.execute("""
-            SELECT id, name FROM urls
-            ORDER BY id DESC""")
+        SELECT urls.id, urls.name as ulr, url_checks.created_at as last_check
+        FROM urls
+        LEFT JOIN url_checks ON urls.id = url_checks.url_id
+        ORDER BY urls.id DESC""")
         all_urls = cursor.fetchall()
         return render_template('urls/urls.html',
                                 urls=all_urls)
@@ -58,7 +67,9 @@ def show_url(id):
         SELECT id, created_at FROM url_checks
         WHERE url_id=%s; """, (id,))
         url_check = cursor.fetchall()
-        id_check, created_at_check = url_check[0]
+        id_check, created_at_check = ('', '')
+        if url_check:
+            id_check, created_at_check = url_check[0]
         return render_template(
             'urls/show_url.html',
             id_url=id,
@@ -68,16 +79,17 @@ def show_url(id):
             created_at_check=created_at_check,
         )
 
-@app.get('urls/<id>/checks')
+
+@app.post('/urls/<id>/checks')
 def check_url(id):
     conn = psycopg2.connect(DATABASE_URL)
     with conn.cursor() as cursor:
         cursor.execute("""
-        INSERT INTO urls (url_id,created_at)
+        INSERT INTO url_checks (url_id,created_at)
         VALUES (%s, %s);
-        """, (id, datetime.today()))
+        """, (id, date.today()))
         conn.commit()
-    return redirect(url_for('show_url'), code=200)
+    return redirect(url_for('show_url', id=id), code=200)
 
 
 
